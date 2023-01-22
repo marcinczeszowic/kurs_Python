72f737ed6cd4183baa6efc2f0b29e3ef9e078d9a
Marcin Czeszowic <marcinczeszowic@gmail.com>
2023-01-22 18:21:15 +0100

    lekcja 52

--- enumeration.py
+++ enumeration.py
@@ -1,32 +1,34 @@
-
+# enumeration - spis - wyliczenie
 import polaFigurDef
+from enum import IntEnum
 
-a = polaFigurDef.pole_kwadratu(2)
-print(a)
+Menu_Figury = IntEnum('Menu_Figury', {'Kwadrat' :1, 'Prostokat' :2, 'Kolo' :3, 'Trojkat' :4, 'Trapez' :5})
 
-wybor = input("""Wybierz figurę , której chcesz pole obliczyć: 
+
+wybor = int(input("""Wybierz figurę , której chcesz pole obliczyć: 
 1. Kwadrat
 2. Prostokąt
 3. Koło
 4. Trójkąt
 5. Trapez
-""")
+"""))
 
-if (wybor == '1'):
+
+if (wybor == Menu_Figury.Kwadrat):
     a = float(input('Podaj bok kwadratu: '))
     print(f"pole kwadratu wynosi: {polaFigurDef.pole_kwadratu(a)}")
-elif (wybor == '2'):
+elif (wybor == Menu_Figury.Prostokat):
     a = float(input("Podaj 1-szy bok prostokąta: "))
     b = float(input("Podaj 2-gi bok prostokąta: "))
     print("Pole prostokąta wynosi:", polaFigurDef.pole_prostokata(a, b))
-elif (wybor == '3'):
+elif (wybor == Menu_Figury.Kolo):
     r = float(input("Podaj promień koła: "))
     print("Pole koła wynosi:", polaFigurDef.pole_kola(r))
-elif (wybor == '4'):
+elif (wybor == Menu_Figury.Trojkat):
     a = float(input("Podaj bok trójkąta: "))
     h = float(input("Podaj wysokość trójkąta: "))
     print("Pole trójkąta wynosi:", polaFigurDef.pole_trojkata(a, h))
-elif (wybor == '5'):
+elif (wybor == Menu_Figury.Trapez):
     a = float(input("Podaj 1-szy bok prostokąta: "))
     b = float(input("Podaj 2-gi bok prostokąta: "))
     h = float(input("Podaj wysokość trapezu: "))
